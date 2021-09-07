0922a034860c055fabe3c56fa8bfa5e4e2b9b371
dk2n16 <dk2n16@soton.ac.uk>
2021-09-07 12:41:30 +0100

    Tests and utilities for polygonisation and joining pop table

--- aggreunit/util_functions.py
+++ aggreunit/util_functions.py
@@ -7,3 +7,65 @@ import geopandas as gpd
 import numpy as np
 import pandas as pd
 import rasterio
+from rasterio.features import shapes
+
+def raster_to_polygon(raster, out_shp=None):
+    """
+    Returns GeoDataFrame of polygonisation of raster and saves GeoDataFrame to out_shp
+
+    Parameters:
+    ------------
+    raster: (Path/str)
+        Path to input raster to polygonise
+    out_shp:    (Path/str) - Optional: Default = None
+        Path to output polygone shape
+
+    Returns:
+    --------
+    gdf :   (gpd.GeoDataFrame)
+        Geodataframe of polygonised version of input raster 
+    """
+    mask = None
+    with rasterio.Env():
+        with rasterio.open(raster) as src:
+            image = src.read().astype(np.int32)# first band
+            mask = image != src.nodata
+            results = ({'properties': {'adm_id': v}, 'geometry': s}   for i, (s, v) in enumerate(shapes(image, mask=mask, transform=src.transform)))
+            geoms = list(results)
+    gdf = gpd.GeoDataFrame.from_features(geoms, crs='EPSG:4326').dissolve(by='adm_id')
+    gdf = gdf.reset_index()
+    if out_shp:
+        if not out_shp.exists():
+            gdf.to_file(out_shp, index=False)
+        else:
+            raise FileExistsError(f"{out_shp} exists. Please delete before creating new shapefile")
+    return gdf
+
+def join_population_to_shp(shp, csv, shp_id='adm_id', csv_id='GID', pop_col='P_2020'):
+    """Joins population csv to shapefile and returns geodataframe -- Overwrites input shp (if it's a file and not a dataframe) with population column from table
+    
+    Parameters:
+    ------------
+    shp:    (Path/str/GeoDataframe)
+        Path to admin unit shapefile or Geodataframe object of shapefile
+    csv :   (Path/str)
+        Path to population table
+    shp_id  :   (str)
+        Column in shapefile used for join (Default -> 'adm_id')
+    csv_id  :   (str)
+        Column in csv used for join (Default -> 'GID')
+
+    """
+    if not isinstance(shp, gpd.GeoDataFrame):
+        gdf = gpd.read_file(shp)
+    else:
+        gdf = shp
+    gdf = gdf[[x for x in gdf.columns if not x == pop_col]].set_index(shp_id)
+    df = pd.read_csv(csv)
+    df[shp_id] = df[csv_id]
+    df_cols = [shp_id, pop_col]
+    df = df[df_cols].set_index(shp_id)
+    gdf_pop = gpd.GeoDataFrame(gdf.join(df))
+    if not isinstance(shp, gpd.GeoDataFrame):
+        gdf_pop.to_file(shp)
+    return gdf_pop
--- tests/test_util_functions.py
+++ tests/test_util_functions.py
@@ -1,10 +1,65 @@
 from pathlib import Path
 
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+from pandas.testing import assert_frame_equal
 import pytest
+import rasterio
 
-from aggreunit import raster_to_polygon
+from aggreunit import raster_to_polygon, join_population_to_shp
 
 BASE = Path(__file__).resolve().parent.joinpath('data')
+
+#----------------INPUT------------------
 L1 = BASE.joinpath('abw_subnational_admin_2000_2020.tif')
 TABLE = BASE.joinpath('abw_population_2000_2020.csv')
 PIXEL = BASE.joinpath('abw_px_area_100m.tif')
+#----------------INPUT------------------
+
+#---------------OUTPUT--------------------
+L1_SHP = L1.parent.joinpath(f'{L1.stem}.shp')
+
+#---------------OUTPUT--------------------
+
+
+def test_files_exist():
+    assert L1.exists()
+    assert TABLE.exists()
+    assert PIXEL.exists()
+
+def test_raster_to_polygon():
+    gdf = raster_to_polygon(L1)
+    src = rasterio.open(L1)
+    data = src.read()
+    data = data != src.nodata
+    units_expected = sorted(list(np.unique(data)))
+    units_got = sorted(gdf.adm_id.to_list())
+    assert isinstance(gdf, gpd.GeoDataFrame)
+
+def test_raster_to_polygon_save_shp():
+    if L1_SHP.exists():
+        [x.unlink() for x in BASE.iterdir() if x.stem == L1.stem if not x == L1]
+    gdf = raster_to_polygon(L1, L1_SHP)
+    gdf_saved = gpd.read_file(L1_SHP)
+    gdf_adm = sorted(gdf.adm_id.to_list())
+    shp_adm = sorted(gdf_saved.adm_id.to_list())
+    assert gdf_adm == shp_adm
+
+def test_join_population_to_shp_with_shp():
+    gdf_pop = join_population_to_shp(L1_SHP, TABLE)
+    assert isinstance(gdf_pop, gpd.GeoDataFrame)
+    assert 'P_2020' in gdf_pop.columns
+
+def test_join_population_to_shp_with_gdf():
+    if L1_SHP.exists():
+        [x.unlink() for x in BASE.iterdir() if x.stem == L1.stem if not x == L1]
+    gdf = raster_to_polygon(L1, L1_SHP)
+    gdf_pop = join_population_to_shp(gdf, TABLE, pop_col='P_2019')
+    assert isinstance(gdf_pop, gpd.GeoDataFrame)
+    assert 'P_2019' in gdf_pop.columns
+
+    
+
+
+
